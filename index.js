0854577a6927bc260a00975b4ae15e5e4ed42f0c
JonasJ <117071036+sajonasJ@users.noreply.github.com>
2024-07-24 22:48:32 +1000

    added adding function

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-const greet = ()=> console.log("Hello World");
-
+"use strict"
+const greet = () => console.log("Hello World");
+const adf = (x, y) => x + y;
 greet();
+
+const addFunc = adf(5, 10);
+console.log(addFunc);
